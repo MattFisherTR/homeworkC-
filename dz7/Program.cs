f9a555ca5818ee4b88d84bcaf0b046d7d70fc27a
Matt <dmitrijdeputatov952@gmail.com>
2022-12-16 15:47:43 +0300

    dz7

---
+++ dz7/Program.cs
@@ -0,0 +1,17 @@
+﻿int numberA = new Random().Next(10, 99);
+Console.WriteLine(numberA);
+
+if (numberA > 10 && numberA < 99)
+{
+if (numberA / 10 > numberA % 10)
+{
+        Console.WriteLine("Первая цифра больше");
+}
+else
+if (numberA / 10 == numberA % 10)
+{
+        Console.WriteLine("Цифры одинаковы");
+}
+else
+Console.WriteLine("Вторая цифра больше");
+}
