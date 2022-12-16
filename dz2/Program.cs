97101059175671e20a109bc4712542b1dd818e4e
Matt <dmitrijdeputatov952@gmail.com>
2022-12-16 15:10:47 +0300

    dz2.2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите номер дня недели");
+﻿Console.Write("Введите номер дня недели ");
 int day = int.Parse(Console.ReadLine()!);
 int a = 1;
 int b = 2;
@@ -7,26 +7,19 @@ int d = 4;
 int f = 5;
 int g = 6;
 int i = 7;
-if (day == a){
-    Console.WriteLine("Понедельник");
-}
-if (day == b){
-    Console.WriteLine("Вторник");
-}
-if (day == c){
-    Console.WriteLine("Среда");
-}
-if (day == d){
-    Console.WriteLine("Четверг");
-}
-if (day == f){
-    Console.WriteLine("Пятница");
-}
-if (day == g){
-    Console.WriteLine("Суббота");
-}
-if (day == i){
-    Console.WriteLine("Воскресенье");
-}
-Console.WriteLine(day);
+if (day == a)
+{Console.WriteLine("Понедельник");}
+if (day == b)
+{Console.WriteLine("Вторник");}
+if (day == c)
+{Console.WriteLine("Среда");}
+if (day == d)
+{Console.WriteLine("Четверг");}
+if (day == f)
+{Console.WriteLine("Пятница");}
+if (day == g)
+{Console.WriteLine("Суббота");}
+if (day == i)
+{Console.WriteLine("Воскресенье");}
+
 
