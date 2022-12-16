7a743014f91060da9dbd96261027ed033831f911
Matt <dmitrijdeputatov952@gmail.com>
2022-12-16 13:57:14 +0300

    dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. По двум введённым числам проверять является ли первое квадратом второго 
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Проверим является ли первое число квадратом второго");
+if (b*b==a){
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else {
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
+
+
+
+
