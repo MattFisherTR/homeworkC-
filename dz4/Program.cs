2adc9cfc4e1c82b827bd49355d481dce117d2bfd
Matt <dmitrijdeputatov952@gmail.com>
2022-12-16 15:15:25 +0300

    dz4

---
+++ dz4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a%2==0)
+{
+    Console.WriteLine("четное число ");
+}
+else
+{
+    Console.WriteLine("нечетное число ");
+}
+
+
