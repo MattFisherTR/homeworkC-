973abf7844d4176f9ffb4a96e481a80e46956ee2
Matt <dmitrijdeputatov952@gmail.com>
2022-12-16 15:48:27 +0300

    dz9

---
+++ dz9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+Double FN = Double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+Double SN = Double.Parse(Console.ReadLine()!);
+
+
+   if ( FN % SN == 0 )
+   {
+        Console.Write("Первое число кратно второму");
+   }
+   else
+   {
+    Console.WriteLine(FN/SN);
+    Console.WriteLine("Число не кратно");
+   }
